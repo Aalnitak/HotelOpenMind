5a0a8e69fe32aea2b71283d02fd3aec9572c4e7a
ljrojas <ljrojas@prebsubner>
2019-03-27 18:53:19 -0300

    borrar comentarios

--- src/controlador/ControlMenu_Principal.java
+++ src/controlador/ControlMenu_Principal.java
@@ -32,26 +32,9 @@ public class ControlMenu_Principal {
         
         jdbcInforme.llenarTablaHabitacionOcupada(tabla);
         
-//        DefaultTableModel model = (DefaultTableModel) tabla.getModel();
-//        System.out.println(model.getColumnCount());
-//        
-//        // 1. llenar nombres habitaciones ocupadas en la tabla
-//
-//        
-//        jdbcHabitacion.mapHabitacionOcupada.keySet().forEach((name) -> {
-//           // Falta cambiar los demás parámetros por info recuperada de queris
-//           model.addRow(new Object[] {name, "2", "jasslkj", "123"});
-//           
-//        });
     
     }
-        
-        // 2. Recuperar idreserva a partir de id_habitación donde idjornada sea máximo
-        // 3. Buscar n_pasajeros a partir de idjornada
-        // 4. Recuperar "última acción" a partir de la tabla reserva_has_producto donde la id_reserva_has_producto
-        //      sea la mayor para la idjornada (o la fecha sea la mayor)
-        //      aplicar formato a la query sql { "Pagó "-inserte producto- " a las "-inserte hora }
-        // 5. Recuperar hora de término y calcular cuenta regresiva para desocuparse
+       
     
 }
   
